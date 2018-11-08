05f138d058d0ba34218c3cdb6f5081bb91946d7b
bartvanhoey <bartvanhoey@hotmail.com>
2018-11-08 21:41:17 +0100

    end part 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListEmployeesComponent } from './employees/list-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent }
+  { path: '', redirectTo: '/list', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+}
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -2,7 +2,6 @@ import { Employee } from './../models/employee.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-employees',
   templateUrl: './list-employees.component.html',
   styleUrls: ['./list-employees.component.css']
 })
