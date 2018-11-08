da4e4c9c25f73828cbe1f1deef5d31dafceda9a4
bartvanhoey <bartvanhoey@hotmail.com>
2018-11-08 22:07:41 +0100

    end part 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ListEmployeesComponent } from './employees/list-employees.component';
 
 const routes: Routes = [
   { path: 'list', component: ListEmployeesComponent },
-  { path: 'create', component: CreateEmployeeComponent }
+  { path: 'create', component: CreateEmployeeComponent },
   { path: '', redirectTo: '/list', pathMatch: 'full'}
 ];
 
