d825be03fa7d0cfd4656300d09aa8d3fa597dfb3
bartvanhoey <bartvanhoey@hotmail.com>
2019-01-03 19:36:49 +0100

    end part 36 ngOnchanges vs property setter

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,18 +1,17 @@
 import { Employee } from './../models/employee.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display-employee',
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
+  @Input() employeeId: number         ;
   private _employee: Employee;
   @Input()
   set employee(value: Employee) {
-    console.log('Previous: ' ,  this._employee ? this._employee.name : 'NULL');
     this._employee = value;
-    console.log('Current: ' ,  this._employee.name );
   }
   get employee(): Employee {
     return this._employee;
@@ -23,6 +22,13 @@ export class DisplayEmployeeComponent implements OnInit {
   ngOnInit() {
   }
 
-
-
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propertyName of Object.keys(changes)) {
+      // console.log('property: ' ,  propertyName);
+      const change = changes[propertyName];
+      const from = JSON.stringify(change.previousValue);
+      const to = JSON.stringify(change.currentValue);
+      console.log(`changed from ${from} to ${to}`);
+    }
+  }
 }
