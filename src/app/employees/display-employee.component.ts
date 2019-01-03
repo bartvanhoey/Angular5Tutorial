52d5ce15cfa1a67ddd72264d6a5d7d758e11a235
bartvanhoey <bartvanhoey@hotmail.com>
2019-01-03 19:41:09 +0100

    end part 36 bis 5h29m19s

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,34 +1,16 @@
 import { Employee } from './../models/employee.model';
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-display-employee',
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
-  @Input() employeeId: number         ;
-  private _employee: Employee;
-  @Input()
-  set employee(value: Employee) {
-    this._employee = value;
-  }
-  get employee(): Employee {
-    return this._employee;
-  }
+export class DisplayEmployeeComponent implements OnInit {
+  @Input() employee: Employee;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    for (const propertyName of Object.keys(changes)) {
-      // console.log('property: ' ,  propertyName);
-      const change = changes[propertyName];
-      const from = JSON.stringify(change.previousValue);
-      const to = JSON.stringify(change.currentValue);
-      console.log(`changed from ${from} to ${to}`);
-    }
   }
 }
